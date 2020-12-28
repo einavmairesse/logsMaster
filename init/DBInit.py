bc43af4e22e96f1eeaa0c1055237b40fb9be0a65
einavmairesse <einavmyress22@gmail.com>
2020-12-28 12:30:20 +0200

    install docker need to be fixed

--- init/DBInit.py
+++ init/DBInit.py
@@ -1,5 +1,19 @@
 import docker
-client = docker.from_env()
+import subprocess as sp
 
-client.containers.run("mysql:8.0.21", None, ports={'3306/tcp': 3306}, name="logs-master-db",
-                      environment=["MYSQL_ROOT_PASSWORD=password"])
+
+class DBInit:
+    def __init__(self, ip):
+        self.ip = ip
+
+    def start_db(self):
+        self._install_docker_py()
+
+    def _install_docker_py(self):
+       print("install docker- demo")
+
+    def _run_mysql_container(self):
+        client = docker.from_env()
+        client.containers.run("mysql:8.0.21", None, ports={'3306/tcp': 3306}, name="logs-master-db",
+                              environment=["MYSQL_ROOT_PASSWORD=password"])
+
